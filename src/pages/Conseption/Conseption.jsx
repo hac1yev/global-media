49d280f4fc2932556e28cb7f2da503663efd3bf9
hac1yev <ilkinhaciyev955@gmail.com>
2023-07-22 15:36:47 +0400

    program concept

--- src/pages/Conseption/Conseption.jsx
+++ src/pages/Conseption/Conseption.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { useDispatch,useSelector } from 'react-redux';
 import { fetchData } from '../../api/fetchData';
 import { navPdfSliceAction } from '../../store/navPdf';
+import { Document, Page,pdfjs } from 'react-pdf';
 import './Conseption.css'
 
 const Concept = () => {
@@ -9,14 +10,18 @@ const Concept = () => {
     const concept = useSelector(state => state.pdfReducer.concept);
     const lang = useSelector(state => state.langReducer.lang);
 
+    pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
     useEffect(() => {
         fetchData(!lang ? 'az/nav' : 'en/nav').then((data) => (
           dispatch(navPdfSliceAction.getConceptPdf(data))
         ))
       },[dispatch,lang])
 
+
   return (
-    <embed title='Concept' src={concept} className='concept-pdf' frameborder="0"></embed>
+    // <embed title='Concept' src={concept} className='concept-pdf' frameborder="0"></embed>
+    <Document file={concept}></Document>
   );
 };
 
